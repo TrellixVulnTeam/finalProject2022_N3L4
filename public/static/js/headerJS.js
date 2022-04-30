c02e59f4a125601beead4e815aeef7b8f2328e42
proexidno <fierydragoon6@gmail.com>
2022-04-30 18:30:45 +0700

    30.04

--- public/static/js/headerJS.js
+++ public/static/js/headerJS.js
@@ -14,8 +14,8 @@ $("#searchForm").click( function () {
     if ($(this).children("button").attr("class") != "btn btn-outline-success"){
         console.log(1);
         $(this).children("button").remove()
-        $(this).append($("<input>").attr("class", "form-control me-2").attr("type", "search").attr("placeholder", "Search").attr("aria-label", "Search").focus())
-        $(this).append($("<button>").attr("class", "btn btn-outline-success").attr("type", "submit").attr("name", "search").text("Search"))
+        $(this).append($("<input>").attr("class", "form-control me-2").attr("type", "search").attr("placeholder", "Search").attr("name", "search").attr("aria-label", "Search").focus())
+        $(this).append($("<button>").attr("class", "btn btn-outline-success").attr("type", "submit").text("Search"))
     }
 })
 $("#profile").hover(function () {
