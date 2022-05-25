5a8b1ad9ea8b4dda15f32f7c8016a119294a5cdb
proexidno <fierydragoon6@gmail.com>
2022-05-26 02:17:40 +0700

    25.05(final)

--- public/static/js/signUpJS.js
+++ public/static/js/signUpJS.js
@@ -1,14 +1,10 @@
 $("#anotherWay").click( function () {
-    $("main form>input").each( function (index)  {
-        switch (index) {
-            case 0:
-                $(this).attr("placeholder", $(this).attr("placeholder") == "Почта" ? "Телефон" : "Почта")
-                break;
-            case 1:
-                $(this).attr("placeholder", $(this).attr("placeholder") == "Пароль" ? "Код" : "Пароль")
-                $("#sendCodeAgain").toggleClass("d-none")
-                break
-        }
-    })
+    $("main form>input:nth-child(2)").attr("placeholder", $("main form>input:nth-child(2)").attr("placeholder") == "Телефон" ? "Почта" : "Телефон")
+    $("form>input:nth-last-child(2)").prop("required", $("form>input:nth-last-child(2)").prop("required") == true ? false :  true)
+    $("form>input:nth-last-child(2)").toggleClass("d-none")
+    $("form>input:nth-last-child(3)").prop("required", $("form>input:nth-last-child(2)").prop("required") == true ? false :  true)
+    $("form>input:nth-last-child(3)").toggleClass("d-none")
+    $("#sendCodeAgain").css("top", $("#sendCodeAgain").css("top") == "-175px" ? "-373px" : "-175px")
+    $("main>div>div>div:last-child").css("height", $("main>div>div>div:last-child").css("height") == "633.965px" ? "704px" : "auto")
     $(this).html($(this).html() == "Создать по почте" ? "Создать по номеру телефона" : "Создать по почте")
 })
