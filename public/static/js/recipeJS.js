2e7f214373a79c5f08495ea1de3bff86c40532b9
proexidno <fierydragoon6@gmail.com>
2022-05-28 00:21:04 +0700

    27.05(final)

--- public/static/js/recipeJS.js
+++ public/static/js/recipeJS.js
@@ -13,3 +13,15 @@ $(window).resize( function () {
     $(".dishImgDiv>img").toggleClass("w-100")
     $(".dishImgDiv>img").css("transform", `translateY(${($(".dishImgDiv").css("height").split("px")[0] - $(".dishImgDiv>img").css("height").split("px")[0]) / 2}px)`)
 })
+
+axios.get('/recipe', {
+    params: {
+        "r": document.location.href.split("r=")[1].split("&")[0]
+    }
+})
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
